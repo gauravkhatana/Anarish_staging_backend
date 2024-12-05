43b03b6dc88177977f337288cbf3d4a1e7602ca9
Kumar Gaurav <gaurav.khatana@anarish.com>
2024-12-05 16:43:46 +0530

    third commit

--- utils/emailService.js
+++ utils/emailService.js
@@ -1,3 +1,52 @@
+// const nodemailer = require("nodemailer");
+// const dotenv = require("dotenv");
+
+// dotenv.config();
+
+// // Configure nodemailer transporter
+// const transporter = nodemailer.createTransport({
+//   host: "anarish.com",
+//   port: 587,
+//   secure: false, // true for 465, false for other ports
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+//   tls: {
+//     rejectUnauthorized: false,
+//   },
+//   pool: true, // Use connection pooling for better performance
+//   maxConnections: 5, // Maximum number of concurrent connections
+//   // connectionTimeout: 10000, // Increase timeout (in ms)
+//   // greetingTimeout: 20000, // Increase greeting timeout (in ms)
+// });
+
+// const sendEmail = (to,cc, subject, html) => {
+//   const mailOptions = { 
+//     from: { name: "Anarish Innovations", address: process.env.EMAIL_USER },
+//     to,
+//     cc,
+//     subject,
+//     html,
+//   };
+//   console.log("email send called");
+//   try {
+//     transporter.sendMail(mailOptions);
+//   } catch (err) {
+//     console.error("Error sending email:", err);
+//   }
+//   // return transporter.sendMail(mailOptions)
+//   // .then(info => {
+//   //   console.log("Email sent: " + info.response);
+//   // })
+//   // .catch(error => {
+//   //   console.error("Error sending email:", error);
+//   //   throw error;
+//   // });
+// };
+
+// module.exports = sendEmail;
+
 const nodemailer = require("nodemailer");
 const dotenv = require("dotenv");
 
@@ -21,7 +70,8 @@ const transporter = nodemailer.createTransport({
   // greetingTimeout: 20000, // Increase greeting timeout (in ms)
 });
 
-const sendEmail = (to,cc, subject, html) => {
+// Make the sendEmail function async to allow using await
+const sendEmail = async (to, cc, subject, html) => {
   const mailOptions = { 
     from: { name: "Anarish Innovations", address: process.env.EMAIL_USER },
     to,
@@ -29,20 +79,17 @@ const sendEmail = (to,cc, subject, html) => {
     subject,
     html,
   };
+
   console.log("email send called");
+  
   try {
-    transporter.sendMail(mailOptions);
+    // Using await here since this function is now async
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Email sent: " + info.response);
   } catch (err) {
     console.error("Error sending email:", err);
   }
-  // return transporter.sendMail(mailOptions)
-  // .then(info => {
-  //   console.log("Email sent: " + info.response);
-  // })
-  // .catch(error => {
-  //   console.error("Error sending email:", error);
-  //   throw error;
-  // });
+
 };
 
 module.exports = sendEmail;
